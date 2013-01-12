9dab728eac56be8957840a323ff4e7c685e7f8af
Eran Hammer <eran@hueniverse.com>
2013-01-12 12:18:34 -0800

    initial commit

---
+++ test/index.js
@@ -0,0 +1,132 @@
+// Load modules
+
+var Chai = require('chai');
+var Boom = process.env.TEST_COV ? require('../lib-cov') : require('../lib');
+
+
+// Declare internals
+
+var internals = {};
+
+
+// Test shortcuts
+
+var expect = Chai.expect;
+
+
+describe('Boom', function () {
+
+    it('returns an error with info when constructed using another error and message', function (done) {
+
+        var err = new Boom(new Error('inner'), 'outter');
+        expect(err.message).to.equal('inner');
+        expect(err.info).to.equal('outter');
+        done();
+    });
+
+    describe('#badRequest', function () {
+
+        it('returns a 400 error code', function (done) {
+
+            expect(Boom.badRequest().code).to.equal(400);
+            done();
+        });
+
+        it('sets the message with the passed in message', function (done) {
+
+            expect(Boom.badRequest('my message').message).to.equal('my message');
+            done();
+        });
+    });
+
+    describe('#unauthorized', function () {
+
+        it('returns a 401 error code', function (done) {
+
+            expect(Boom.unauthorized().code).to.equal(401);
+            done();
+        });
+
+        it('sets the message with the passed in message', function (done) {
+
+            expect(Boom.unauthorized('my message').message).to.equal('my message');
+            done();
+        });
+
+        it('returns a WWW-Authenticate header when passed a scheme', function (done) {
+
+            var err = Boom.unauthorized('boom', 'Test');
+            expect(err.code).to.equal(401);
+            expect(err.toResponse().headers['WWW-Authenticate']).to.equal('Test');
+            done();
+        });
+    });
+
+    describe('#forbidden', function () {
+
+        it('returns a 403 error code', function (done) {
+
+            expect(Boom.forbidden().code).to.equal(403);
+            done();
+        });
+
+        it('sets the message with the passed in message', function (done) {
+
+            expect(Boom.forbidden('my message').message).to.equal('my message');
+            done();
+        });
+    });
+
+    describe('#notFound', function () {
+
+        it('returns a 404 error code', function (done) {
+
+            expect(Boom.notFound().code).to.equal(404);
+            done();
+        });
+
+        it('sets the message with the passed in message', function (done) {
+
+            expect(Boom.notFound('my message').message).to.equal('my message');
+            done();
+        });
+    });
+
+    describe('#internal', function () {
+
+        it('returns a 500 error code', function (done) {
+
+            expect(Boom.internal().code).to.equal(500);
+            done();
+        });
+
+        it('sets the message with the passed in message', function (done) {
+
+            expect(Boom.internal('my message').message).to.equal('my message');
+            done();
+        });
+
+        it('passes data on the callback if its passed in', function (done) {
+
+            expect(Boom.internal('my message', { my: 'data' }).data.my).to.equal('data');
+            done();
+        });
+    });
+
+    describe('#toResponse', function () {
+
+        it('formats a custom error', function (done) {
+
+            var err = new Boom(500, 'Unknown');
+            err.toResponse = function () {
+
+                return { payload: { test: true } };
+            };
+
+            expect(err.toResponse().payload.test).to.equal(true);
+            done();
+        });
+    });
+});
+
+
